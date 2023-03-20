abdcff5a573ba3d71459aee3e74f8b7e627de0ba
Carsten Tang <carsten@ninjageeks.pl>
2023-03-20 09:08:54 +0100

    Added comments to example

--- examples/connection_demo.cpp
+++ examples/connection_demo.cpp
@@ -4,22 +4,27 @@ using namespace zutano;
 
 auto main() -> int {
 
+    // Initialize the client for ArangoDB.
     auto conn = connection()
-            .endpoint("http://192.168.1.10")
-            .auth("root", "123");
+            .endpoint("http://localhost:8529")
+            .auth("root", "passwd");
 
+    // Connect to "_system" database as root user.
     auto sys_db = conn.database("_system");
 
+    // Create a new database named "demo".
     sys_db.create_datatabase("demo");
 
+    // Connect to "demo" database as root user.
     auto db = conn.database("demo");
 
-    db.create_collection("students");
+    // Create a new collection named "students".
+    auto students = db.create_collection("students");
 
-    auto students = db.collection("students");
-
+    // Add a hash index to the collection.
     students.add_hash_index({.fields= {"name"}, .unique= true});
 
+    // Insert new documents into the collection.
     students.insert({{"name", "jane"},
                      {"age",  39}});
     students.insert({{"name", "josh"},
@@ -27,6 +32,7 @@ auto main() -> int {
     students.insert({{"name", "judy"},
                      {"age",  21}});
 
+    // Execute an AQL query and iterate through the result cursor.
     auto cursor = db.execute("FOR doc IN students RETURN doc");
 
     for (auto doc: cursor) {
